0378bbd2de38b30e7c8ad082dc787108d7b00777
Giacomo Leidi <giacomo.leidi@uni-bielefeld.de>
2021-01-06 03:08:06 +0100

    tweetskb_to_tsv: Save tweet ids for scraping.

--- bin/tweetkb_to_tsv.sh
+++ bin/tweetkb_to_tsv.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-[[ "$#" -ne 1 ]] && echo "Usage $(basename $0) /path/to/dir." && exit 1
+[[ "$#" -ne 1 ]] && echo "Usage $(basename "$0") /path/to/dir." && exit 1
 
 tweets_dir="${1%/}"
 entities_db_dir="${tweets_dir}/db"
-
-mkdir -p "${entities_db_dir}"
+tweet_ids="${entities_db_dir}/ids.tsv"
 
 while read -r line;
 do
-    entity=$(echo "$line" | sed -E "s/(^.*_:| rdf:type.*)//g")
+    entity=$(printf "%s\n" "$line" | sed -E "s/(^.*_:| rdf:type.*)//g")
 
-    tweet_id=$(echo "$entity" | sed -E "s/(e|_.*)//g")
+    tweet_id=$(printf "%s\n" "$entity" | sed -E "s/(e|_.*)//g")
+    printf "%s\n" "$tweet_id"  >> "${tweet_ids}"
 
-    uri=$(echo "$line" | sed -E "s/(.*<|>.*)//g")
+    uri=$(printf "%s\n" "$line" | sed -E "s/(.*<|>.*)//g")
+    printf "%s\n" "$uri"  >> "${tweets_dir}/t${tweet_id}.tsv"
 
-    token=$(echo "$line" | sed -E "s/^.+dAs (.+) ; nee:has.+$/\1/")
+    token=$(printf "%s\n" "$line" | sed -E "s/^.+dAs \"(.+)\" ; nee:has.+$/\1/")
 
-    echo -e "t${tweet_id}\t${entity}\t${uri}\t${token}" | \
-        tee -a "${entities_db_dir}/tweets_and_entities.tsv" | \
-        cut -f 3 >> "${tweets_dir}/t${tweet_id}.tsv"
+    printf "t%s\t%s\t%s\n" "${tweet_id}" "${uri}" "${token}" >> "${entities_db_dir}/tweets_and_entities.tsv" 
 done

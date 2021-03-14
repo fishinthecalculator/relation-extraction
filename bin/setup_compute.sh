0e437fa98c3b9a1d31ff03b3186ecd124638649a
Giacomo Leidi <giacomo.leidi@uni-bielefeld.de>
2021-03-14 23:09:41 +0100

    setup_compute.sh: Extract tarball from nfs volume.

--- bin/setup_compute.sh
+++ bin/setup_compute.sh
@@ -4,15 +4,13 @@ set -euo pipefail
 set -x
 
 remote_dir="${HOME}/remote"
-thesis_dir="${HOME}/thesis"
 tarball="$(basename "$(find "${remote_dir}/" -name "*.tar.gz" | head -1)")"
 
-rm -rf "${thesis_dir}/"*.tar.gz
-mv "${remote_dir}/${tarball}" "${thesis_dir}/"
 
 cd "${HOME}/.local/relext/"
 
 chmod -R 777 ./*
 rm -rf ./*
 
-tar -xvf "${thesis_dir}/${tarball}"
+tar -xvf "${remote_dir}/${tarball}"
+rm -rf "${remote_dir}/${tarball}"

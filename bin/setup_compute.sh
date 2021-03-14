ee4a12c1f58c54b5c2a541f8a79424ae6b1a1750
Giacomo Leidi <giacomo.leidi@uni-bielefeld.de>
2021-03-14 21:02:28 +0100

    setup_compute.sh: Install updated tools.

--- bin/setup_compute.sh
+++ bin/setup_compute.sh
@@ -9,3 +9,10 @@ thesis_dir="${HOME}/thesis"
 
 rm -rf "${thesis_dir}/*.tar.gz"
 mv "${remote_dir}/*.tar.gz" "${thesis_dir}/"
+
+cd "${HOME}/.local/relext/"
+
+chmod -R 777 ./*
+rm -rf ./*
+
+tar -xvf "$(find "$thesis_dir" -name "*.tar.gz" | head -1)"

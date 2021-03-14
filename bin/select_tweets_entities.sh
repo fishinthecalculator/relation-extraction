d77f163320a205dd15a53a457e7cbb88ec1ea5dc
Giacomo Leidi <giacomo.leidi@uni-bielefeld.de>
2021-03-14 19:58:19 +0100

    select_tweets_entities: Perform 2*N_PROCS jobs.

--- bin/select_tweets_entities.sh
+++ bin/select_tweets_entities.sh
@@ -5,11 +5,13 @@ set -euo pipefail
 
 results_dir="${2%/}"
 ids="${results_dir}/tweets/ids.tsv"
+procs="$(nproc --all)"
+n_jobs="$(( procs * 2 ))"
 
 echo "Saving ids to ${ids}..."
 
 rg "rdf:type nee:Entity" "${1}" | \
-    parallel -N1 --linebuffer --roundrobin --pipe "$(dirname "$0")/tweetkb_to_tsv.sh \"${results_dir}\"" | \
+    parallel -N1 --linebuffer --roundrobin --jobs "$n_jobs" --pipe "$(dirname "$0")/tweetkb_to_tsv.sh \"${results_dir}\"" | \
     tee /dev/fd/2 | \
     sort -n -u > "${ids}"
 
